4c8231a7210b6220e3b13fee7cd71c6246eec390
Levi Baber <baberlevi@gmail.com>
2016-10-26 13:41:00 -0500

    removing app.py ;

--- app.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.insert(0, '/usr/local/bin/google_appengine')
-sys.path.insert(0, '/usr/local/bin/google_appengine/lib')
-sys.path.insert(0, '/var/www/html/touchterrain')
-
-from TouchTerrain_app import app as application
