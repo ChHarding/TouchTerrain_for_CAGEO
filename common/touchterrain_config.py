f81fe4474c7e81dea758bbbacf0949e1b620f5c2
Chris Harding <charding@iastate.edu>
2018-02-14 11:52:49 -0600

    contains touchterrain specific settings/constants

---
+++ common/touchterrain_config.py
@@ -0,0 +1,20 @@
+# TouchTerrain server config settings
+
+
+# Defaults
+
+# type of server:
+SERVER_TYPE = "Apache" # "paste" or "Apache"
+#SERVER_TYPE = "paste" # so I can run the server inside a debugger, needs to run with single core!
+
+# multiprocessing:
+NUM_CORES = 0 # 0 means: use all cores
+if SERVER_TYPE == "paste": NUM_CORES = 1 # 1 means don't use multi-core at all
+
+# when to use tempfiles instead of memory to store processed tiles
+MAX_CELLS =   1000 * 1000  # if DEM has > this number of cells, use tempfile instead of memory
+
+#--------------------------------------------------------------------------------
+
+# Overrides
+MAX_CELLS = 0  # CH: test to force using tempfiles
