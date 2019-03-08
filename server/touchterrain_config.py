88fe5e90e728458764c2ebd0b15d95c9586977f8
Chris <charding@iastate.edu>
2019-03-08 13:04:32 -0600

    updated limits (for ISU server)

--- server/touchterrain_config.py
+++ server/touchterrain_config.py
@@ -11,13 +11,17 @@ SERVER_TYPE = "Apache" # "paste" or "Apache"
 NUM_CORES = 0 # 0 means: use all cores
 if SERVER_TYPE == "paste": NUM_CORES = 1 # 1 means don't use multi-core at all
 
-# when to use tempfiles instead of memory to store processed tiles
-MAX_CELLS = 500 * 500  # if DEM has > this number of cells, use tempfile instead of memory
+# for STL/OBJ don't even start with a DEM bigger than that number. GeoTiff export is this * 50!
+MAX_CELLS_PERMITED =   1000 * 1000 * 0.4   
 
-MAX_CELLS_PERMITED = 1000 * 1000 * 2 # for STL/OBJ don't even start with a DEM bigger than that number. GeoTiff export is this * 50!
+# if DEM has > this number of cells, use tempfile instead of memory
+MAX_CELLS = MAX_CELLS_PERMITED / 4  
+
 
 TMP_FOLDER = "tmp" # local to this folder! for temp files and final zip
 #--------------------------------------------------------------------------------
 
 # Overrides
 #MAX_CELLS = 0  # CH: test to force using tempfiles
+~                                                                                                                  
+~                                                       
