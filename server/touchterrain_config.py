29b0c31cddff4ec7776c1e3a626bd275939b2e8a
Chris <charding@iastate.edu>
2019-03-07 18:11:05 -0600

    change quotas to work with the new system

--- server/touchterrain_config.py
+++ server/touchterrain_config.py
@@ -12,9 +12,9 @@ NUM_CORES = 0 # 0 means: use all cores
 if SERVER_TYPE == "paste": NUM_CORES = 1 # 1 means don't use multi-core at all
 
 # when to use tempfiles instead of memory to store processed tiles
-MAX_CELLS = 1000 * 1000  # if DEM has > this number of cells, use tempfile instead of memory
+MAX_CELLS = 500 * 500  # if DEM has > this number of cells, use tempfile instead of memory
 
-MAX_CELLS_PERMITED = 1000 * 1000 * 5  # for STL/OBJ don't even start with a DEM bigger than that number. GeoTiff export is this * 50!
+MAX_CELLS_PERMITED = 1000 * 1000 * 2 # for STL/OBJ don't even start with a DEM bigger than that number. GeoTiff export is this * 50!
 
 TMP_FOLDER = "tmp" # local to this folder! for temp files and final zip
 #--------------------------------------------------------------------------------
