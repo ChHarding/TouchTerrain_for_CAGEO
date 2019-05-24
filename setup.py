9e5a56308c2535aef9bbd099554c541582b9baf3
ChHarding <charding@iastate.edu>
2019-05-24 18:43:27 -0500

    no  google api modules needed

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
         'Pillow>=6.0.0',
         'earthengine-api>=0.1.175',
         'Flask>=1.0.2',
-        #'google-api-python-client>=1.7.8',
         'vectormath>=0.2.2',
         'oauth2client>=4.1.3',
     ],
