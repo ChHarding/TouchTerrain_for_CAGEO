6cf75dc217537b0665bbb03cbf4997f775247117
Nick Booher <njbooher@iastate.edu>
2019-05-24 11:11:27 -0500

    Pull keys and stuff from environment if available

--- server/config.py
+++ server/config.py
@@ -7,18 +7,21 @@ from ee import oauth
 #from oauth2client.appengine import AppAssertionCredentials
 
 # The URL of the Earth Engine API.
-EE_URL = 'https://earthengine.googleapis.com'
+EE_URL = os.getenv('TOUCHTERRAIN_EE_URL', 'https://earthengine.googleapis.com')
 
 # The service account email address authorized by your Google contact.
 # Set up a service account as described here:
 # https://sites.google.com/site/earthengineapidocs/creating-oauth2-service-account
-EE_ACCOUNT = 'earthengine@touchterrain.iam.gserviceaccount.com'
+EE_ACCOUNT = os.getenv('TOUCHTERRAIN_EE_ACCOUNT', 'earthengine@touchterrain.iam.gserviceaccount.com')
 
 # The private key associated with your service account in Privacy Enhanced
 # Email format (.pem suffix).  To convert a private key from the RSA format
 # (.p12 suffix) to .pem, run the openssl command like this:
 # openssl pkcs12 -in downloaded-privatekey.p12 -nodes -nocerts > privatekey.pem
-EE_PRIVATE_KEY_FILE = 'server/privatekey.pem'
+EE_PRIVATE_KEY_FILE = os.getenv('TOUCHTERRAIN_EE_PRIVATE_KEY_FILE', 'server/privatekey.pem')
+
+# Location of the file containing the google maps key
+GOOGLE_MAPS_KEY_FILE = os.getenv('TOUCHTERRAIN_GOOGLE_MAPS_KEY_FILE', 'GoogleMapsKey.txt')
 
 # DEBUG_MODE will be True if running in a local development environment.
 DEBUG_MODE = ('SERVER_SOFTWARE' in os.environ and
