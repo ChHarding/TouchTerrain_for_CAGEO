2a6bdc92979b33141244cdf334a21068d2d2d8d1
Chris <charding@iastate.edu>
2019-06-07 16:59:04 -0500

    removed debug server run script

--- Run_TouchTerrain_debug_server_app.py
+++
@@ -1,6 +0,0 @@
-# This simply runs TouchTerrain_app.py in the server folder
-# Use this for debugging only! (Should not even be on github!)
-
-from server.TouchTerrain_app import app
-app.run(debug=False, port=8080)
-print("done with TouchTerrain_server_app.py")
