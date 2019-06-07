574918b912f87133bc272b687f2465a3d1be9edf
Nick Booher <njbooher@iastate.edu>
2019-06-07 14:05:06 -0500

    fakeout

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     dependency_links=[
-        'https://github.com/allelos/vectors/tarball/master#egg=vectors-1.0.0'
+        'https://github.com/allelos/vectors/tarball/master#egg=vectors-99.0.0'
     ],
     install_requires=[
         'Jinja2>=2.10.1',
         'Pillow>=6.0.0',
         'earthengine-api>=0.1.178',
         'Flask>=1.0.2',
-        'vectors==1.0.0',
+        'vectors==99.0.0',
         'vectormath>=0.2.2',
         'oauth2client>=4.1.3',
     ],
