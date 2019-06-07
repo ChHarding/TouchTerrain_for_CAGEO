0adef4f20225b2264a7a601be3aa2eae5d8410df
Nick Booher <njbooher@iastate.edu>
2019-06-07 13:47:47 -0500

    Try that

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='touchterrain',
-    dependency_links=[
-        'https://github.com/allelos/vectors/tarball/master#egg=package-1.0'
-    ],
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
+        'package @ git+https://github.com/allelos/vectors.git@master#egg=package-1.0.0',
         'Jinja2>=2.10.1',
         'Pillow>=6.0.0',
         'earthengine-api>=0.1.178',
