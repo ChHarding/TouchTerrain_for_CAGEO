925812ec0119eef3771b453617340f05fc8ff67b
ChHarding <charding@iastate.edu>
2019-06-26 14:41:01 -0500

    added comment

--- server/gunicorn_settings.py
+++ server/gunicorn_settings.py
@@ -1,1 +1,4 @@
-timeout = 120
+# Needed to avoid timing out for long-ish jobs (> 30 secs?)
+# Note that if you've got another router in front of this, like haproxy or heroku or openshift, 
+# you might need to change the timeout there too
+timeout = 120 
