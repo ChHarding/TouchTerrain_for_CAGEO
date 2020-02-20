b398a6574dfb6a7a8be5520b45b0cfd246d01253
Chris <charding@iastate.edu>
2020-02-20 15:31:23 -0600

    upped use memory threshold to 100%

--- server/config.py
+++ server/config.py
@@ -21,13 +21,14 @@ NUM_CORES = 0 # 0 means: use all cores
 if SERVER_TYPE == "flask_local": NUM_CORES = 1 # 1 means don't use multi-core at all
 
 
-# limits for ISU server 
+# limits for ISU server
 
 # for STL/OBJ don't even start with a DEM bigger than that number. GeoTiff export is this * 100!
-MAX_CELLS_PERMITED =   1000 * 1000 * 0.567  
+MAX_CELLS_PERMITED =   1000 * 1000 * 0.567
 
 # if DEM has > this number of cells, use tempfile instead of memory
-MAX_CELLS = MAX_CELLS_PERMITED / 4  
+#MAX_CELLS = MAX_CELLS_PERMITED / 4
+MAX_CELLS = MAX_CELLS_PERMITED # CH: testing if tempfile doesn't work anymore?
 
 
 TMP_FOLDER = os.getenv('TOUCHTERRAIN_TMP_FOLDER', os.path.join(config.SERVER_DIR, "tmp"))
@@ -36,7 +37,7 @@ DOWNLOADS_FOLDER = os.getenv('TOUCHTERRAIN_DOWNLOADS_FOLDER', os.path.join(confi
 
 PREVIEWS_FOLDER = os.getenv('TOUCHTERRAIN_PREVIEWS_FOLDER', os.path.join(config.SERVER_DIR, "previews"))
 
-# This will be inlined in index.html to enable Google Analytics, However, this is  
+# This will be inlined in index.html to enable Google Analytics, However, this is
 # my tracking id, so if you use google analytics, make sure to use your own Tracking ID!
 GOOGLE_ANALYTICS_TRACKING_ID = "UA-93016136-1"
 # If you don't wan to use GA, just comment out the line above
