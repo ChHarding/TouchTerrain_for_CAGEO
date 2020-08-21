7fc14672eba51f6785fc8cc77bf3030f407ade38
Chris Harding <charding@iastate.edu>
2020-08-21 17:40:40 -0500

    used to test gxp, just uncommented arg setup for now

--- TouchTerrain_standalone.py
+++ TouchTerrain_standalone.py
@@ -157,6 +157,43 @@ def main():
         for k in overwrite_args:
             args[k] = overwrite_args[k]
        
+    # CH testing gpx here, so I can use the debugger
+    '''
+    args = {
+                "importedDEM": None,
+                "DEM_name": "USGS/NED",   # DEM source
+                # area for gpx test
+                "bllat": 39.32205105794382,   # bottom left corner lat
+                "bllon": -120.37497608519418, # bottom left corner long
+                "trlat": 39.45763749030933,   # top right corner lat
+                "trlon": -120.2002248034559, # top right corner long
+                "tilewidth": 120,  # width of each tile in mm,  
+                "printres": 0.4,  # resolution (horizontal) of 3D printer in mm
+                "ntilesx": 1, # number of tiles in x  
+                "ntilesy": 1, # number of tiles in y    
+                "basethick": 0.5,   # thickness (in mm) of printed base
+                "zscale": 1.5,  # elevation (vertical) scaling
+                "fileformat": "STLb",  # format of 3D model file
+                "zip_file_name": "test_get_zipped_tiles_gpx",   # base name of zipfile, .zip will be added
+                "importedGPX": # Plot GPX paths from these files onto the model.
+                            ["stuff/gpx-test/DLRTnML.gpx",
+                            "stuff/gpx-test/DonnerToFrog.gpx",
+                            "stuff/gpx-test/CinTwistToFrog.gpx",
+                            "stuff/gpx-test/sagehen.gpx",
+                            "stuff/gpx-test/dd-to-prosser.gpx",
+                            "stuff/gpx-test/alder-creek-to-crabtree-canyon.gpx",
+                            "stuff/gpx-test/ugly-pop-without-solvang.gpx",
+                            "stuff/gpx-test/tomstrail.gpx"   
+                            ],
+                "gpxPathHeight": 100,  # Currently we plot the GPX path by simply adjusting the raster elevation at the specified lat/lon,
+                                    # therefore this is in meters. Negative numbers are ok and put a dent in the mdoel  
+                "gpxPixelsBetweenPoints" : 20, # GPX Files haves a lot of points. A higher number will create more space between lines drawn
+                                                # on the model and can have the effect of making the paths look a bit cleaner 
+                "gpxPathThickness" : 5, # Stack parallel lines on either side of primary line to create thickness. 
+                                        # A setting of 1 probably looks the best 
+    }
+    '''
+
     # print out current args 
     print("\nUsing these config values:")
     for k in sorted(args.keys()):
