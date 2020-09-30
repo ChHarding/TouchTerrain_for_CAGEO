363a8425ef869dd8fbb9904ec8716ee30e7783de
Chris Harding <charding@iastate.edu>
2020-09-30 16:56:16 -0500

    make place markers bounceable

--- touchterrain/server/templates/touchterrain.js
+++ touchterrain/server/templates/touchterrain.js
@@ -67,6 +67,7 @@ window.onload = function () {
     map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
     // Create the search box and link it to the UI element.
+    //https://developers.google.com/maps/documentation/javascript/examples/places-searchbox#maps_places_searchbox-html
     const input = document.getElementById("pac-input");
     const searchBox = new google.maps.places.SearchBox(input);
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
@@ -79,50 +80,60 @@ window.onload = function () {
     // more details for that place.
     searchBox.addListener("places_changed", () => {
         const places = searchBox.getPlaces();
-
-        if (places.length == 0) {
-        return;
-        }
+        if (places.length == 0) {return}
+        
         // Clear out the old markers.
         markers.forEach((marker) => {
-        marker.setMap(null);
+            marker.setMap(null);
         });
         markers = [];
+
         // For each place, get the icon, name and location.
         const bounds = new google.maps.LatLngBounds();
         places.forEach((place) => {
-        if (!place.geometry) {
-            console.log("Returned place contains no geometry");
-            return;
-        }
-        const icon = {
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(25, 25),
-        };
-        // Create a marker for each place.
-        markers.push(
-            new google.maps.Marker({
-            map,
-            icon,
-            title: place.name,
-            position: place.geometry.location,
-            })
-        );
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            const icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25),
+            };
+            // Create a marker for each place.
+            markers.push(
+                new google.maps.Marker({
+                map,
+                icon,
+                title: place.name,
+                position: place.geometry.location,
+                animation: google.maps.Animation.DROP,
+                animation: google.maps.Animation.BOUNCE,
+                })
+            );
+            // add a callback for bounce on/off
+            markers[markers.length-1].addListener("click", toggleMarkerBounce);
 
-        if (place.geometry.viewport) {
-            // Only geocodes have viewport.
-            bounds.union(place.geometry.viewport);
-        } else {
-            bounds.extend(place.geometry.location);
-        }
-        });
+            function toggleMarkerBounce(){
+                if (markers[markers.length-1].getAnimation() !== null) {
+                    markers[markers.length-1].setAnimation(null);
+                } else {
+                    markers[markers.length-1].setAnimation(google.maps.Animation.BOUNCE);
+                }
+            }
+              
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+            });
+
         map.fitBounds(bounds);
     });
-
-
 
 
     eemap = create_overlay(MAPID, map); // (global) hillshade overlay from google earth engine
