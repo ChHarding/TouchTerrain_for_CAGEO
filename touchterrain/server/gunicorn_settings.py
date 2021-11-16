e712d7ef949890daf2d9843f6170cee840fd9f93
Nick Booher <njbooher@iastate.edu>
2021-11-16 15:48:54 -0600

    update for new python container

--- touchterrain/server/gunicorn_settings.py
+++ touchterrain/server/gunicorn_settings.py
@@ -2,3 +2,7 @@
 # Note that if you've got another router in front of this, like haproxy or heroku or openshift, 
 # you might need to change the timeout there too
 timeout = 5 * 60
+
+# required for newer python container
+accesslog = '-'
+bind = ['0.0.0.0:8080']
