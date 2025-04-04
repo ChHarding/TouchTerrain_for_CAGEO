b5be7206ecd1c16775b2f4cb7ee7cc7a8a6a7b46
ChHarding <charding@iastate.edu>
2025-04-04 16:05:15 -0500

    Change full config URL from link to simple tes

--- touchterrain/server/TouchTerrain_app.py
+++ touchterrain/server/TouchTerrain_app.py
@@ -621,12 +621,13 @@ def export():
             
             html += "   <br><br>If you take picture of your touchterrain 3D prints (or CNC carves) and put them on Instagram why not tag them with #touchterrain?"
 
-            html += "   <br><br>To return to the selection map, click on the back button in your browser once, or on the link below:<br>"
+            html += "   <br><br>To return to the selection map, click on the back button in your browser once<br>"
             #html += "<br>Click on the URL below to return to the selection map:<br>"
 
             # print out the query parameter URL 
-            html += '<a href = "'
-            html += URL_query_str + '">' + URL_query_str + "</a><br>"
+            #html += '<a href = "'
+            #html += URL_query_str + '">' + URL_query_str + "</a><br>"
+            html += URL_query_str # using non-link for know as bots may follow it 
             html += "<br>To have somebody else generate the same model, have them copy&paste this URL into a browser<br>" 
  
             # set timout flag to true, so the timeout script doesn't fire
